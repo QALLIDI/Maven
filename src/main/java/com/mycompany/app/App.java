30184d5a8d680747bf741b025e33e07e3c3459ea
Qallidi Mohammed AMine <qallidimohammedamine@gmail.com>
2018-12-18 12:34:03 +0000

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,7 @@ public class App
     private final String message = "Hello World!";
     private final String coucou = "coucou";
     private final String coucou2 = "coucou 2";
+    private final String coucou3 = "coucou 3";
     public App() {}
 
     public static void main(String[] args) {
